bcb7b151d16ad2e36aa203867c7b204adccaefd5
Ajay Narayanan <ajay.narayanan.zx@gmail.com>
2024-05-29 19:30:00 +0200

    datasets shell script added, Readme file replaced, datasets folder added to gitignore

---
+++ install_datasets.sh
@@ -0,0 +1,10 @@
+mkdir datasets
+cd datasets
+
+# Clone the  grade-school-math repository
+git clone https://github.com/openai/grade-school-math.git
+
+# Clone the BigBench Hard Repository
+git clone https://github.com/suzgunmirac/BIG-Bench-Hard.git
+
+
