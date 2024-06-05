ebea98c7e7b29070320c4252956f440ff241321c
Ajay Narayanan <ajay.narayanan.zx@gmail.com>
2024-06-05 13:11:18 +0200

    added results dir method to project utils, more bbh code added

--- examples/bigbench_hard/bigbench_hard.py
+++ examples/bigbench_hard/bigbench_hard.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 from typing import Dict, List, Callable, Union, Any
 from graph_of_thoughts import controller, language_models, operations, prompter, parser
-import project_utils as Project
+import project_utils as project
 
 TASKS: list[str] = ["boolean_expressions", "causal_judgement", "date_understanding", "disambiguation_qa",
                     "dyck_languages",
@@ -53,7 +53,7 @@ class BigBenchHardPrompter(prompter.Prompter):
         :rtype: str
         :raise AssertionError: If the requested number of branches is not one.
         """
-        prompt_path = Project.datasets_dir() / "BIG-Bench-Hard" / "cot-prompts" / f"{task}.txt"
+        prompt_path = project.datasets_dir() / "BIG-Bench-Hard" / "cot-prompts" / f"{task}.txt"
         full_prompt: str = ""
         with open(prompt_path, "r") as f:
             for _ in range(2):  # to skip canary warning
@@ -70,18 +70,18 @@ class BigBenchHardPrompter(prompter.Prompter):
                 elif method.startswith("cot"):
                     full_examples.append(example)
                 else:
-                    logging.error("Unknown method %s", method)
-                    full_prompt = "ERROR: Unknown method {}".format(method) # TODO only here for testing, should be removed later
-                    return full_prompt
+                    raise ValueError(f"Unknown method: {method}")
 
                 full_examples.append(self.answer_prompt.format(answer=answers[i]))
-            if method.startswith("io"): # TODO consider making methods an enum?
-                full_prompt = self.io_prompt.format(instruction=prompt, examples="\n".join(full_examples), input="input")
+            if method.startswith("io"):
+                full_prompt = self.io_prompt.format(instruction=prompt, examples="\n".join(full_examples),
+                                                    input="input")
             elif method.startswith("cot"):
-                full_prompt = self.io_prompt.format(instruction=prompt, examples="\n".join(full_examples), input="input")
+                full_prompt = self.io_prompt.format(instruction=prompt, examples="\n".join(full_examples),
+                                                    input="input")
             else:
-                logging.error("Unknown method %s", method)
-                full_prompt = "ERROR: Unknown method {}".format(method)
+                raise ValueError(f"Unknown method: {method}")
+
         return full_prompt
 
     def aggregation_prompt(self, state_dicts: List[Dict], **kwargs) -> str:
@@ -97,6 +97,79 @@ class BigBenchHardPrompter(prompter.Prompter):
         pass
 
 
+class BigBenchHardParser(parser.Parser):
+    """
+    BigBenchHardParser provides the parsing of the language model responses to the BigBench-Hard Dataset.
+
+    Inherits from the Parser class and implements its abstract methods.
+    """
+
+    def parse_generate_answer(self, state: Dict, texts: List[str]) -> List[Dict]:
+        """
+        Parse the response from the language model for a generate prompt.
+
+        :param state: The thought state used to generate the prompt.
+        :type state: Dict
+        :param texts: The responses to the prompt from the language model.
+        :type texts: List[str]
+        :return: The new thought states after parsing the response from the language model.
+        :rtype: List[Dict]
+        """
+        raise NotImplementedError("This method needs to be implemented.")
+
+    def parse_aggregation_answer(self, response: str, **kwargs) -> Union[Dict, List[Dict]]:
+        pass
+
+    def parse_improve_answer(self, response: str, **kwargs) -> Dict:
+        pass
+
+    def parse_validation_answer(self, response: str, **kwargs) -> bool:
+        pass
+
+    def parse_score_answer(self, response: str, **kwargs) -> List[float]:
+        pass
+
+
+def io() -> operations.GraphOfOperations:
+    """
+    Generates the Graph of Operations for the IO method.
+
+    :return: Graph of Operations
+    :rtype: GraphOfOperations
+    """
+    operations_graph = operations.GraphOfOperations()
+
+    operations_graph.append_operation(operations.Generate(1, 1))
+    operations_graph.append_operation(operations.GroundTruth(NotImplemented))
+
+    return operations_graph
+
+
+def cot() -> operations.GraphOfOperations:
+    """
+    Generates the Graph of Operations for the COT method.
+
+    :return: Graph of Operations
+    :rtype: GraphOfOperations
+    """
+    operations_graph = operations.GraphOfOperations()
+
+    operations_graph.append_operation(operations.Generate(1, 1))
+    operations_graph.append_operation(operations.GroundTruth(NotImplemented))
+
+    return operations_graph
+
+
+def cot_sc() -> operations.GraphOfOperations:
+    """
+    Generates the Graph of Operations for the COT-SC method.
+
+    :return: Graph of Operations
+    :rtype: GraphOfOperations
+    """
+    raise NotImplementedError("This method needs to be implemented.")
+
+
 def run(
         data_ids: List[int],
         methods: List[Callable[[], operations.GraphOfOperations]],
@@ -108,9 +181,83 @@ def run(
     if not tasks:
         logging.info("No tasks specified, selecting all tasks")
         tasks.extend(TASKS)
+    else:
+        logging.info("Running the following tasks: %s", tasks)
 
+    # create the results directory
+    config = {
+        "tasks": tasks,
+        "methods": [method.__name__ for method in methods],
+        "lm": lm_name,
+        "budget": budget,
+    }
+
+    # create directory for results
+    results_dir = project.create_results_dir(
+        directory=os.path.dirname(__file__),
+        lm_name=lm_name,
+        methods=methods,
+        config=config,
+        tasks=tasks
+    )
+
+    datasets_dir: Path = project.datasets_dir() / "BIG-Bench-Hard" / "bbh"
+    for task in tasks:
+        logging.info(f"Evaluating task: {task}")
+        task_data_path: Path = datasets_dir / f"{task}.json"
+        task_results_dir = results_dir / task
+        with open(task_data_path, "r") as f:
+            task_data = json.load(f)["examples"]  # we load the entire json at once as it seems the files are not too big.
+            for id, example in enumerate(task_data):
+                for method in methods:
+                    method_results_dir = task_results_dir / method.__name__
+                    logging.info(f"Running method {method.__name__}")
+                    logging.info(f"Budget left: {budget}")
+                    if budget <= 0.0:
+                        logging.error(
+                            f"Budget has been depleted, stopping. Method {method.__name__} has not been run."
+                        )
+                        break
+                    lm = language_models.ChatGPT(
+                        os.path.join(
+                            os.path.dirname(__file__),
+                            "../../graph_of_thoughts/language_models/config.json",
+                        ),
+                        model_name=lm_name,
+                        cache=True,
+                    )
+                    operations_graph = method()
+                    executor = controller.Controller(
+                        lm,
+                        operations_graph,
+                        BigBenchHardPrompter(),
+                        BigBenchHardParser(),
+                        {
+                            "input": example["input"],
+                            "ground_truth": example["target"],
+                            "current": "",
+                            "phase": 0,
+                            "method": method.__name__
+                        }
+                    )
+                    try:
+                        executor.run()
+                    except Exception as e:
+                        logging.error(f"Exception: {e}")
+                    output_path:Path = method_results_dir / f"{id}.json"
+                    executor.output_graph(str(output_path))
+                    budget -= lm.cost
+
+    return orig_budget - budget
 
 if __name__ == "__main__":
-    prompter = BigBenchHardPrompter()
-    for task in TASKS:
-        print(prompter.generate_prompt(1, method="io", task=task))
+    budget = 30
+    samples = [item for item in range(5)]
+    approaches = [io]
+
+    spent = run(samples, approaches, budget, "llama3-8b-ollama")
+
+    logging.info(f"Spent {spent} out of {budget} budget.")
+    # prompter = BigBenchHardPrompter()
+    # for task in TASKS:
+    #     print(prompter.generate_prompt(1, method="io", task=task))
--- project_utils.py
+++ project_utils.py
@@ -1,3 +1,7 @@
+import json
+import logging
+import os
+from datetime import datetime
 from pathlib import Path
 
 
@@ -9,3 +13,43 @@ def project_dir() -> Path:
 def datasets_dir() -> Path:
     """Returns the path of the `datasets` directory."""
     return Path(__file__).resolve().parent / "datasets"
+
+
+def create_results_dir(directory: str, lm_name: str, methods: [str], config: dict, tasks: [str] = []) -> Path:
+    """
+    Creates the results directory for the given dataset and methods.
+    :param directory: The directory inside which the results directory is created.
+    :param lm_name: The name of the LM used.
+    :param methods: The list of methods used.
+    :param config: Other configuration data for the results.
+    :param tasks: The list of tasks used. leave empty if single task or no subdirectories are required for different tasks.
+    :return: The path to the created results directory.
+    """
+
+    results_dir = os.path.join(directory, "results")
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    extra_info = f"{lm_name}_{'-'.join([method.__name__ for method in methods])}"
+    folder_name = f"{extra_info}_{timestamp}"
+    results_folder = os.path.join(results_dir, folder_name)
+    os.makedirs(results_folder)
+    with open(os.path.join(results_folder, "config.json"), "w") as f:
+        json.dump(config, f)
+
+    logging.basicConfig(
+        filename=os.path.join(results_folder, "log.log"),
+        filemode="w",
+        format="%(name)s - %(levelname)s - %(message)s",
+        level=logging.DEBUG,
+    )
+    if tasks:
+        for task in tasks:
+            for method in methods:
+                # create a results directory for the task and method
+                os.makedirs(os.path.join(results_folder, task, method.__name__))
+    else:
+        for method in methods:
+            # create a results directory for the method
+            os.makedirs(os.path.join(results_folder, method.__name__))
+    return Path(results_folder)
