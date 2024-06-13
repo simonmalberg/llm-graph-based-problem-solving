2dc61566e207bfd749a9465e3bac80ad12c8a060
Ajay Narayanan <ajay.narayanan.zx@gmail.com>
2024-06-13 14:38:32 +0200

    Setup to download CommonSenseQA Dataset

--- install_datasets.sh
+++ install_datasets.sh
@@ -7,4 +7,6 @@ git clone https://github.com/openai/grade-school-math.git
 # Clone the BigBench Hard Repository
 git clone https://github.com/suzgunmirac/BIG-Bench-Hard.git
 
-
+# Download the CommonSenseQA Dataset
+mkdir CommonSenseQA
+curl -O https://s3.amazonaws.com/commensenseqa/train_rand_split.jsonl --output-dir CommonSenseQA/
